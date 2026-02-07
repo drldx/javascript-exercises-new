d2d3389bf8d3008f0fc3ef5204be680b206b99fa
drldx <168914904+drldx@users.noreply.github.com>
2026-02-07 12:40:03 +0530

    add: factorial and contains solution

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,30 @@
-const contains = function() {
-  
+const contains = function(obj, val) {
+
+  for (const key in obj) {
+
+    const valuesArr = Object.values(obj);
+
+    if(obj[key] === val) return true;
+    if(valuesArr.includes(val)) return true;
+
+    if(typeof(obj[key]) === 'object') {
+
+      // if(Array.isArray(obj[key])) {
+      //
+      //   for(let i of obj[key]) {
+      //     contains(i, val);
+      //   }
+      //
+      // } else {
+        if(contains(obj[key], val)) return true;
+      // }
+    } 
+    // else {
+    //   console.log(key + ':' + obj[key] );
+    // }
+  }
+  return false;
 };
-  
-// Do not edit below this line
+
+// do not edit below this line
 module.exports = contains;
