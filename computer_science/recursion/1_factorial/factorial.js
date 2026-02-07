3a1f0fe9ae91fbefe9d7bae879b1a49e4e96d0ff
drldx <168914904+drldx@users.noreply.github.com>
2026-02-07 12:41:14 +0530

    add; factorial solution

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,4 +1,9 @@
-const factorial = function() {
+const factorial = function(n) {
+
+  if(n === 0 || n === 1) return 1;
+  if(typeof(n) !== 'number' || n < 1 || (n % 1) !== 0) return undefined;
+  
+  return n * factorial(n - 1);
 
 };
 
